56885770f5f49fe7d6891cea7406d4e7e6f2e455
Harish <harish210430@gmail.com>
2024-11-02 09:52:59 -0700

    scoped variable

---
+++ var_let_Const.js
@@ -0,0 +1,11 @@
+// Difference B/W var, let and const.
+
+// let - we can Re-assign a value after declaration.
+// const- we can not assign/Re-assign value after declaration, It through an Error.
+
+/*
+Scope:
+// let & const: (Block and Function Scoped) -It creats a Scope when they inside a block/Function scope.
+// Var: (Function Scoped)- It creats scope inside function, Otherwise it can not create Scope.
+
+*/
